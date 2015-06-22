48ed846721c3a34b940ed1c37bef493827bf6bf8
NikhilVijayakumar <nikhil.vijaykumar@gmail.com>
2015-06-22 17:25:03 +0530

    init

---
+++ NavigationDrawerLib/src/com/teamnikaml/navigationdrawerlib/model/Mapper.java
@@ -0,0 +1,51 @@
+/**
+ * @author Nikhil V
+ *
+ * Jun 22, 2015
+ */
+package com.teamnikaml.navigationdrawerlib.model;
+
+import java.util.List;
+
+import android.app.Fragment;
+
+/**
+ * @author Nikhil V
+ * 
+ */
+public class Mapper {
+	
+	
+	private static Mapper mapper;
+	
+	public static Mapper getMapper()
+	{
+		if(mapper == null)
+			mapper = new Mapper();
+		return mapper;
+		
+	}
+
+	private List<NavDrawerItem> navigationDrawerItemList;
+	
+	private List<Fragment>fragmentList; 
+
+	public List<NavDrawerItem> getNavigationDrawerItemList() {
+		return navigationDrawerItemList;
+	}
+
+	public void setNavigationDrawerItemList(
+			List<NavDrawerItem> navigationDrawerItemList) {
+		this.navigationDrawerItemList = navigationDrawerItemList;
+	}
+
+	public List<Fragment> getFragmentList() {
+		return fragmentList;
+	}
+
+	public void setFragmentList(List<Fragment> fragmentList) {
+		this.fragmentList = fragmentList;
+	}
+
+	
+}
---
+++ TestNavigationDrawer/src/com/example/testnavigationdrawer/MainActivity.java
@@ -0,0 +1,106 @@
+package com.example.testnavigationdrawer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.example.testnavigationdrawer.fragment.CommunityFragment;
+import com.example.testnavigationdrawer.fragment.FindPeopleFragment;
+import com.example.testnavigationdrawer.fragment.HomeFragment;
+import com.example.testnavigationdrawer.fragment.PagesFragment;
+import com.example.testnavigationdrawer.fragment.PhotosFragment;
+import com.example.testnavigationdrawer.fragment.WhatsHotFragment;
+import com.teamnikaml.navigationdrawerlib.activity.NavigationDrawerActivity;
+import com.teamnikaml.navigationdrawerlib.model.Mapper;
+import com.teamnikaml.navigationdrawerlib.model.NavDrawerItem;
+
+
+
+public class MainActivity extends Activity {
+
+	private List<Fragment> fragmentList;
+
+	private List<NavDrawerItem> navigationDrawerItemList;
+	
+	Mapper mapper = Mapper.getMapper();
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		setData();
+		
+		Intent intent = new Intent(getApplicationContext(),NavigationDrawerActivity.class);
+		startActivity(intent);
+		
+	
+
+	}
+
+	private void setData() {
+		// TODO Auto-generated method stub
+
+		String[] title = { "Home", "Find People", "Photos", "Communities",
+				"Pages", "What\'s Hot" };
+		int[] icon = { R.drawable.ic_home, R.drawable.ic_people,
+				R.drawable.ic_photos, R.drawable.ic_communities,
+				R.drawable.ic_pages, R.drawable.ic_whats_hot };
+
+		navigationDrawerItemList = new ArrayList<>();
+		NavDrawerItem drawerItem;
+
+		for (int i = 0; i < 6; i++) {
+
+			drawerItem = new NavDrawerItem(title[i], icon[i]);
+			if (i == 3) {
+				drawerItem.setCount("22");
+				drawerItem.setCounterVisibility(true);
+			}
+
+			if (i == 5) {
+				drawerItem.setCount("50");
+				drawerItem.setCounterVisibility(true);
+			}
+			navigationDrawerItemList.add(drawerItem);
+		}
+
+		fragmentList = new ArrayList<Fragment>();
+
+		fragmentList.add(new HomeFragment());
+		fragmentList.add(new FindPeopleFragment());
+		fragmentList.add(new PhotosFragment());
+		fragmentList.add(new CommunityFragment());
+		fragmentList.add(new PagesFragment());
+		fragmentList.add(new WhatsHotFragment());
+		
+		
+		mapper.setFragmentList(fragmentList);
+		mapper.setNavigationDrawerItemList(navigationDrawerItemList);
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
